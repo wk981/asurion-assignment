6cf68523131a75aafbde33c3042a057edf786ecf
wk981 <jamesim445@gmail.com>
2024-07-06 22:37:28 +0800

    Remove unnecessary file

--- backend/service/conversation.js
+++
@@ -1,8 +0,0 @@
-let conversation = [
-    {
-      role: 'system',
-      content: "You will follow the conversation and respond to the queries asked by the 'user's content. You will act as the assistant"
-    }
-];
-
-module.exports = conversation;
